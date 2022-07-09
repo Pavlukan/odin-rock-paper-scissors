a2f37ee101470a092a18e75a49859ec3609910a8
Pavlukan <theremustbesomethingsmart@gmail.com>
2022-07-09 12:57:09 +0300

    Create a function which plays one round of the game
    The function has two parameters. It contains 3 if-statements for
    every possible outcome (win, draw, lose).

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+let computerChoice;
+
 // create a function that randomly returns either 'Rock', 'Paper', or 'Scissors'
 function computerPlay() {
 
-    // create a variable that stores a whole number between 1 and 3
-    let computerChoice = Math.floor((Math.random() * 3) + 1);
+    // store a whole number between 1 and 3 in the global variable computerChoice
+    computerChoice = Math.floor((Math.random() * 3) + 1);
 
     if (computerChoice === 1) {
         computerChoice = "Rock";
@@ -16,3 +18,25 @@ function computerPlay() {
 
     return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+
+    // create an if-statement for the tie
+    if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) { 
+        return console.log("Damn, it's a tie!");
+    // create an if-statement for losing
+    } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "PAPER" || 
+    playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSORS" || playerSelection.toUpperCase() === "SCISSORS" &&
+    computerSelection.toUpperCase() === "ROCK") {
+        return console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    // create an if-statement for winning
+    } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS" || 
+    playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK" || playerSelection.toUpperCase() === "SCISSORS" &&
+    computerSelection.toUpperCase() === "PAPER") {
+        return console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    } else {
+        return console.log("Something went terribly wrong...");
+    }
+}
+
+playRound('Rock', computerPlay());
