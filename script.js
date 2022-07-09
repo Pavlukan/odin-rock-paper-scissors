8320e1d614eba1f30a346ff147f8fcb82e91ceb2
Pavlukan <theremustbesomethingsmart@gmail.com>
2022-07-09 13:34:24 +0300

    Make the first letter of user's input uppercase and others lowercase
    Also added a bit of comments to the code.

--- script.js
+++ script.js
@@ -32,31 +32,41 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "PAPER" || 
     playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSORS" || playerSelection.toUpperCase() === "SCISSORS" &&
     computerSelection.toUpperCase() === "ROCK") {
-        ++lose;
+        ++lose; // increment the amount of losses
         return console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
     // create an if-statement for winning
     } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS" || 
     playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK" || playerSelection.toUpperCase() === "SCISSORS" &&
     computerSelection.toUpperCase() === "PAPER") {
-        ++win;
+        ++win; // increment the amount of wins
         return console.log(`You win! ${playerSelection} beats ${computerSelection}`);
     } else {
         return console.log("Something went terribly wrong...");
     }
 }
 
+// create a function which plays five rounds of the game and declares the winner
 function game() {
+
+    // create a loop which will play five rounds of the game
     for (let i = 0; i < 5; i++) {
-        playerSelection = prompt("Choose: Rock, Paper, or Scissors", "");
+        playerChoice = prompt("Choose: Rock, Paper, or Scissors", ""); // allow user to input his weapon
+        playerSelection = playerChoice.toUpperCase().slice(0, 1) + playerChoice.toLowerCase().slice(1);  // make the user input look beautiful by making the first letter uppercase and others lowercase.
         playRound(playerSelection, computerPlay());
     } 
 
+    // create an if-statement for the tie of the whole game
     if (win === lose) {
         console.log("It's a tie, mate :(");
+    // create an if-statement for the win of the whole game
     } else if (win > lose) {
         console.log("You win, motherfucker!");
+    // create an if-statement for losing the whole game
     } else if (win < lose) {
         console.log("You lost to a robot, noob.");
+    // create an else-statement for something horrible... 
+    } else {
+        console.log("Something went terribly wrong...");
     }
 }
 
